7d6187d45fe3d5b8d3c0bb2d3bc6de438c857cea
yhosutun2491 <yhosutun2491@gmail.com>
2022-08-22 17:34:29 +0800

    javascript standard revised and fixed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-//Required framework in the project
+// Required framework in the project
 const express = require('express')
 const app = express()
 const port = 3000
-//Required template-engine
+// Required template-engine
 const exphbs = require('express-handlebars')
-//Restaurant data
+// Restaurant data
 const restaurantList = require('./restaurant.json').results
 // setting template engine and helper function
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
+  defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 // setting static files
@@ -21,41 +21,34 @@ app.get('/restaurants/:res_id', (req, res) => {
   const eachRestaurant = restaurantList.find(item => item.id === Number(req.params.res_id))
   res.render('showRes', { restaurant: eachRestaurant })
 })
-
-
-
 function checkInputLanguage(word) {
   const cnRegex = /[\u4e00-\u9fa5]/g
   const enRegex = /[a-zA-z]/g
   if (word.length !== 0) {
     if ((word.match(cnRegex)) && (word.match(cnRegex).length > 0)) {
-      return "CN"
+      return 'CN'
     }
     if (word.match(enRegex).length > 0) {
-      return "EN"
+      return 'EN'
     }
   }
-
 }
-
-
-//SerachBars
+// SearchBars
 app.get('/search', (req, res) => {
-  //如果輸入是英文搜尋，回傳英文結果
   const words = req.query.keyword.toLowerCase().trim()
   const lang = checkInputLanguage(words)
   let results = []
-  //如果輸入是中文搜尋，回傳中文餐廳結果
-  if (lang === "CN") {
+  // input is chinese return chinese filter results
+  if (lang === 'CN') {
     results = restaurantList.filter(item => item.name.includes(words) || item.category.includes(words))
   }
-  //如果輸入是英文搜尋，回傳英文餐廳結果
-  else if (lang === "EN") {
+  // input is English return English filter results
+  else if (lang === 'EN') {
     results = restaurantList.filter(item => item.name_en.toLowerCase().includes(words))
   }
   res.render('index', { restaurant: results, keyword: words })
 })
-//start and listen on the Express server
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
