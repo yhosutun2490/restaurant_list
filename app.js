393fc11c0d0570ed77531a2e22e7a75397b236d9
yhosutun2491 <yhosutun2491@gmail.com>
2022-08-22 17:04:27 +0800

    finished modification of input search

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const port = 3000
 const exphbs = require('express-handlebars')
 //Restaurant data
 const restaurantList = require('./restaurant.json').results
-// setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// setting template engine and helper function
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}))
 app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
@@ -20,34 +22,38 @@ app.get('/restaurants/:res_id', (req, res) => {
   res.render('showRes', { restaurant: eachRestaurant })
 })
 
+
+
 function checkInputLanguage(word) {
   const cnRegex = /[\u4e00-\u9fa5]/g
   const enRegex = /[a-zA-z]/g
-  if ((word.match(cnRegex)) && (word.match(cnRegex).length > 0)) {
-    return "CN"
+  if (word.length !== 0) {
+    if ((word.match(cnRegex)) && (word.match(cnRegex).length > 0)) {
+      return "CN"
+    }
+    if (word.match(enRegex).length > 0) {
+      return "EN"
+    }
   }
-  if (word.match(enRegex).length > 0) {
-    return "EN"
-  }
+
 }
+
+
 //SerachBars
 app.get('/search', (req, res) => {
   //如果輸入是英文搜尋，回傳英文結果
   const words = req.query.keyword.toLowerCase().trim()
-  if (words.length === 0) {
-    return
-  }
   const lang = checkInputLanguage(words)
   let results = []
   //如果輸入是中文搜尋，回傳中文餐廳結果
   if (lang === "CN") {
     results = restaurantList.filter(item => item.name.includes(words) || item.category.includes(words))
   }
-  //如果輸入是中文搜尋，回傳英文餐廳結果
+  //如果輸入是英文搜尋，回傳英文餐廳結果
   else if (lang === "EN") {
     results = restaurantList.filter(item => item.name_en.toLowerCase().includes(words))
   }
-  res.render('index', { restaurant: results })
+  res.render('index', { restaurant: results, keyword: words })
 })
 //start and listen on the Express server
 app.listen(port, () => {
